1477d33924001257c1b5716a88a11f696e78e822
geonhee <pollux0718@gmail.com>
2026-09-02 14:28:29 +0900

    configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Rigel0718.github.io',
+  base: '/Astro',
+});
